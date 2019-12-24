0fa2e1f7763373870912a2a56e1b404deb9fc990
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-12-24 13:02:53 -0500

    Implement & test Item

--- backend/src/main/kotlin/info/ditrapani/todo/Item.kt
+++ backend/src/main/kotlin/info/ditrapani/todo/Item.kt
@@ -1,13 +1,31 @@
 package info.ditrapani.todo
 
 import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
 
 data class Item(val author: String, val description: String, val status: Status) {
     fun toJson(): JsonObject {
-        return JsonObject()
+        val jsonObj = json {
+            obj(
+                "author" to author,
+                "description" to description,
+                "status" to status.status
+            )
+        }
+        if (status is Done) {
+            jsonObj.put("worker", status.worker)
+        }
+        return jsonObj
     }
 }
 
-sealed class Status
-object Todo : Status()
-data class Done(val worker: String) : Status()
+sealed class Status {
+    abstract val status: String
+}
+object Todo : Status() {
+    override val status = "todo"
+}
+data class Done(val worker: String) : Status() {
+    override val status = "done"
+}
--- backend/src/test/kotlin/info/ditrapani/todo/ItemTest.kt
+++ backend/src/test/kotlin/info/ditrapani/todo/ItemTest.kt
@@ -1,15 +1,37 @@
 package info.ditrapani.todo
 
 import io.kotlintest.specs.FreeSpec
-import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.core.json.json
+import io.vertx.kotlin.core.json.obj
 import kotlin.test.assertEquals
 
 class ItemTest : FreeSpec({
     "toJson" - {
-        "returns json representation of Item" {
-            val actual = Item("John", "Wash car", Todo).toJson()
-            val expected = JsonObject()
-            assertEquals(expected, actual)
+        "returns json representation of Item" - {
+            "when the status is todo" {
+                val actual = Item("John", "Wash car", Todo).toJson()
+                val expected = json {
+                    obj(
+                        "author" to "John",
+                        "description" to "Wash car",
+                        "status" to "todo"
+                    )
+                }
+                assertEquals(expected, actual)
+            }
+
+            "when the status is done" {
+                val actual = Item("John", "Wash car", Done("Luke")).toJson()
+                val expected = json {
+                    obj(
+                        "author" to "John",
+                        "description" to "Wash car",
+                        "status" to "done",
+                        "worker" to "Luke"
+                    )
+                }
+                assertEquals(expected, actual)
+            }
         }
     }
 })
